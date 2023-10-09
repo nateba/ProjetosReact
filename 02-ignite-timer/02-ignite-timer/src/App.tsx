6f66032359413871d00a5934136cc49331339f60
Nathan de Araújo Cunha Lisboa <naclisboa@gmail.com>
2023-10-09 08:37:00 -0300

    att themes

--- 02-ignite-timer/02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/02-ignite-timer/src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 import { Button } from './components/Button';
 
 export function App() {
 
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="sucess" />
 
-    </>
+    </ThemeProvider>
   )
 }
 
--- 02-ignite-timer/02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/02-ignite-timer/src/components/Button.styles.ts
@@ -17,7 +17,5 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 width: 100px;
 height: 40px;
 
-${props => {
-        return css`background-color: ${buttonVariants[props.variant]};`
-    }}
-`
+background-color: ${props => props.theme.secondary};
+    ` 
